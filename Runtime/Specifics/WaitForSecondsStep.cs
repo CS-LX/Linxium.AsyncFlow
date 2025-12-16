6d3c3d6d73b8f20fcabf9b407249fd0754276152
LX.. <1659404657@qq.com>
2025-12-16 14:08:47 +0800

    add WaitForSecondsStep

---
+++ Runtime/Specifics/WaitForSecondsStep.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Scripting;
+
+namespace Linxium.AsyncFlow.Specifics {
+    [Preserve]
+    public class WaitForSecondsStep : IStep {
+        float waitTime;
+
+        public void Initialize(StepContext context) {
+            waitTime = context.GetValue("WaitTime", waitTime);
+        }
+
+        public UniTask ExecuteAsync(CancellationToken cancellationToken) => UniTask.WaitForSeconds(waitTime, cancellationToken: cancellationToken);
+    }
+}
