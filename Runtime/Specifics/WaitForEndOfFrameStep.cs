7cc5a3dd8e5d55f6419849952b1198a7461aa4d5
LX.. <1659404657@qq.com>
2025-12-16 14:20:13 +0800

    add 2 steps

---
+++ Runtime/Specifics/WaitForEndOfFrameStep.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Scripting;
+
+namespace Linxium.AsyncFlow.Specifics {
+    [Preserve]
+    public class WaitForEndOfFrameStep : IStep {
+
+        public void Initialize(StepContext context) {
+        }
+
+        public UniTask ExecuteAsync(CancellationToken cancellationToken) => UniTask.WaitForEndOfFrame(cancellationToken: cancellationToken);
+    }
+}
